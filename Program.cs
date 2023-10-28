8b170944c65c12133b9d56f71c3b5705a6712f63
Ramka <whereisourgod@gmail.com>
2023-10-28 18:13:44 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace C_D0202
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int test = 54;
+            long test2 = 100;
+            byte test3 = 1;
+            Bits any = test;
+            Bits any2 = test2;
+            Bits any3 = test3;
+            Console.WriteLine(any);
+            Console.WriteLine(any2);
+            Console.WriteLine(any3);
+            Console.ReadLine();
+        }
+    }
+}
